3230c323fb3d59da98a6b4c97ea2d790bcda0c2f
diego-rg <76407123+diego-rg@users.noreply.github.com>
2021-09-06 16:52:58 +0200

    first try with onclick

--- js.js
+++ js.js
@@ -1,9 +1,21 @@
 //Select the score of each player and change it to a number, since it was a string!
-let playerOneScore = parseInt(document.querySelector("#player-one-score").innerText);
-let playerTwoScore = parseInt(document.querySelector("#player-two-score").innerText);
+let playerOneScore = document.querySelector("#player-one-score");
+let playerTwoScore = document.querySelector("#player-two-score");
 
-let playerOnePoint = document.querySelector(".button--green");
-playerOnePoint.addEventListener("click", function() {
-    playerOneScore = playerOneScore + 1;
-    )
+//Create the onclick event to add poits each time
+const playerOnePoint = document.querySelector(".button--green");
+playerOnePoint.onclick = function() {
+    playerOneScore.innerText = parseInt(document.querySelector("#player-one-score").innerText) + 1;
+}
+
+const playerTwoPoint = document.querySelector(".button--blue");
+playerTwoPoint.onclick = function() {
+    playerTwoScore.innerText = parseInt(document.querySelector("#player-two-score").innerText) + 1;
+}
+
+//Create the reset button
+const resetScore = document.querySelector(".button--red");
+resetScore.onclick = function() {
+    playerOneScore.innerText = 0;
+    playerTwoScore.innerText = 0;
 }
