20be423ef31936469ce20acac10648b1ba8b93d9
diego-rg <76407123+diego-rg@users.noreply.github.com>
2021-09-06 17:18:48 +0200

    Finished?using addEventListener and value

--- js.js
+++ js.js
@@ -1,16 +1,27 @@
-//Select the score of each player and change it to a number, since it was a string!
+//Select the score of each player.
 let playerOneScore = document.querySelector("#player-one-score");
 let playerTwoScore = document.querySelector("#player-two-score");
+let winningScoreValue = document.querySelector("#winningScore");
 
-//Create the onclick event to add poits each time
+//Create the onclick event to add poits each time and check if a player has reached the winning score.
 const playerOnePoint = document.querySelector(".button--green");
-playerOnePoint.onclick = function() {
+playerOnePoint.addEventListener("click", addPointsOne);
+
+function addPointsOne() {
     playerOneScore.innerText = parseInt(document.querySelector("#player-one-score").innerText) + 1;
+    if (playerOneScore.innerText == winningScoreValue.value) {
+        alert("Player One wins! Reset to start a new game.");
+    }
 }
 
 const playerTwoPoint = document.querySelector(".button--blue");
-playerTwoPoint.onclick = function() {
+playerTwoPoint.addEventListener("click", addPointsTwo);
+
+function addPointsTwo() {
     playerTwoScore.innerText = parseInt(document.querySelector("#player-two-score").innerText) + 1;
+    if (playerTwoScore.innerText == winningScoreValue.value) {
+        alert("Player One wins! Reset to start a new game.");
+    }
 }
 
 //Create the reset button
